5f5c2b3e1ac995a834dec8f02efb18a0248cad7d
Bilal Junaidy <bilal.junaidy@gmail.com>
2020-11-19 10:31:08 -0500

    Additional updated

---
+++ animate.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const H1 = document.querySelector('h1');
+    console.log(H1)
+    H1.style.animationPlayState = 'paused';
+
+    document.querySelector('#bhainsbutton').onclick = function() {
+        if (H1.style.animationPlayState == 'paused') {
+            H1.style.animationPlayState = 'running';
+        }
+
+        else {
+            H1.style.animationPlayState = 'paused';
+        }
+    }
+})
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     // Find heading
+//     const h1 = document.querySelector('h1');
+
+//     // Pause Animation by default
+//     h1.style.animationPlayState = 'paused';
+
+//     // Wait for button to be clicked
+//     document.querySelector('button').onclick = () => {
+
+//         // If animation is currently paused, begin playing it
+//         if (h1.style.animationPlayState == 'paused') {
+//             h1.style.animationPlayState = 'running';
+//         }
+
+//         // Otherwise, pause the animation
+//         else {
+//             h1.style.animationPlayState = 'paused';
+//         }
+//     }
+
+// })
